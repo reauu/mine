40e3ea141796706a6cb887d3f2875e1015b1faba
reauu <znarfieee@gmail.com>
2023-11-30 15:42:07 +0800

    badaboom

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 let bear = ["Grizzly", "Panda", "Ice Bear"];
+let grades = [90, 95];
+let ave = 0;
+
+for (let u = 0 ; u < grades.length ; u++) {
+    ave += grades[i];
+    console.log(ave);
+}
 
 for (let i = 0 ; i < bear.length ; i++) {
     console.log(bear[i]);
 }
+
+console.log()
+console.log();
+console.log();
+console.log();
